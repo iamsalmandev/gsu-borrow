72a71f65259cee3bb0e821af322a04415dc10b9f
iamsalmandev <dev.iamsalman@gmail.com>
2022-08-10 15:50:14 +0500

    updates token icon

--- content/landing.ts
+++ content/landing.ts
@@ -10,9 +10,9 @@ export const LANDING_PILLS = [
     link: '/asset/btc',
   },
   {
-    icon: 'dai_circle_color',
-    label: 'DAI',
-    link: '/asset/dai',
+    icon: 'gsu_circle_color',
+    label: 'GSUC',
+    link: '/asset/gsuc',
   },
   {
     icon: 'uni_lp_circle_color',
